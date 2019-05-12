18810be27c2ff69381df0ec243b5244455286408
Adarsh <aj3120@gmail.com>
2019-05-12 23:16:25 +0530

    redux devtool added

--- src/Redux/createStore.js
+++ src/Redux/createStore.js
@@ -6,10 +6,15 @@ import { routerMiddleware } from 'connected-react-router'
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState) {
+  const composeEnhancers =typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
   const store = createStore(
     createRootReducer(history), // root reducer with router state
     preloadedState,
-    compose(
+    composeEnhancers(
       applyMiddleware(
         routerMiddleware(history), // for dispatching history actions
         reduxlogger// ... other middlewares ...
